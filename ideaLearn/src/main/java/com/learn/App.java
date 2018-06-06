a728d9dbd831e177d241f58dfea17b934128c543
huxb <huxubin@pcidata.cn>
2018-06-06 15:21:27 +0800

    third commit.

--- ideaLearn/src/main/java/com/learn/App.java
+++ ideaLearn/src/main/java/com/learn/App.java
@@ -12,7 +12,7 @@ public class App {
         LocalDateTime ldt = LocalDateTime.now();
         System.out.println(ldt);
         System.out.println("edit by bowser.");
-        Sysout.out.println("modify on github.");
+        System.out.println("modify on github.");
         System.out.println("add by idea.");
     }
 }
